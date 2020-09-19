90b30f459a199b1639c658221b5207ac30a1a3cc
unknown <diego.resendez@yahoo.com>
2020-09-19 15:24:41 -0700

    fixed with help redone npm init

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES("Bacon Cheeseburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES("Philly Cheeseburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES("Black N Blue Cheeseburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES("Vegan Cheeseburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES("Double Bacon Cheeseburger", FALSE);
